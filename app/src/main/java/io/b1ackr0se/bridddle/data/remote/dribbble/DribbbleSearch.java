82d249c6c50cc0137c64449af91743d9f5bd724a
Nguyễn Thanh Hải <haint.fit12@gmail.com>
2016-11-14 10:18:02 +0700

    Setup dribbble search service

---
+++ app/src/main/java/io/b1ackr0se/bridddle/data/remote/dribbble/DribbbleSearch.java
@@ -0,0 +1,35 @@
+package io.b1ackr0se.bridddle.data.remote.dribbble;
+
+
+import android.support.annotation.StringDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.List;
+
+import io.b1ackr0se.bridddle.data.model.Shot;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
+public interface DribbbleSearch {
+
+    String END_POINT = "https://dribbble.com/";
+    String SORT_POPULAR = "";
+    String SORT_RECENT = "latest";
+
+    @GET("search")
+    Observable<List<Shot>> search(@Query("q") String query,
+                                  @Query("page") Integer page,
+                                  @Query("per_page") Integer pageSize,
+                                  @Query("s") @SortingOrder String sort);
+
+    @Retention(RetentionPolicy.SOURCE)
+    @StringDef({
+            SORT_POPULAR,
+            SORT_RECENT
+    })
+    @interface SortingOrder {
+
+    }
+}
