5486edcef8ccc8a39f69ea4069fda0940edfa6dd
Nguyễn Thanh Hải <haint.fit12@gmail.com>
2016-11-21 10:48:45 +0700

    User view and presenter

---
+++ app/src/main/java/io/b1ackr0se/bridddle/ui/user/UserPresenter.java
@@ -0,0 +1,53 @@
+package io.b1ackr0se.bridddle.ui.user;
+
+import javax.inject.Inject;
+
+import io.b1ackr0se.bridddle.base.BasePresenter;
+import io.b1ackr0se.bridddle.data.remote.dribbble.DribbbleApi;
+import io.b1ackr0se.bridddle.util.SharedPref;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
+
+
+public class UserPresenter extends BasePresenter<UserView> {
+
+    private CompositeSubscription compositeSubscription;
+    private DribbbleApi dribbbleApi;
+    private SharedPref sharedPref;
+    private int userId;
+    private int page = 1;
+
+    @Inject
+    public UserPresenter(DribbbleApi dribbbleApi, SharedPref sharedPref) {
+        this.dribbbleApi = dribbbleApi;
+        this.sharedPref = sharedPref;
+        this.compositeSubscription = new CompositeSubscription();
+    }
+
+    void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    void loadShots(boolean firstPage) {
+        if (firstPage) {
+            page = 1;
+        }
+        compositeSubscription.add(
+                dribbbleApi.getUserShots(userId)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread()).subscribe(
+                        shots -> {
+                            if (shots.isEmpty()) {
+                                getView().showEmptyShot();
+                            } else {
+                                getView().showShots(shots);
+                                page++;
+                            }
+                        },
+                        throwable -> getView().showError()
+                ));
+    }
+
+
+}
