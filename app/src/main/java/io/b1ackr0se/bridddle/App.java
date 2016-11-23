e2c96c214dc0bb4c966510ea29eb032ace6f6a4a
Nguyễn Thanh Hải <haint.fit12@gmail.com>
2016-11-23 14:44:19 +0700

    Fabric integration

--- app/src/main/java/io/b1ackr0se/bridddle/App.java
+++ app/src/main/java/io/b1ackr0se/bridddle/App.java
@@ -4,19 +4,27 @@ package io.b1ackr0se.bridddle;
 import android.app.Application;
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
+
 import io.b1ackr0se.bridddle.injection.components.AppComponent;
 import io.b1ackr0se.bridddle.injection.components.DaggerAppComponent;
 import io.b1ackr0se.bridddle.injection.modules.AppModule;
+import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
     AppComponent appComponent;
 
-    @Override public void onCreate() {
+    @Override
+    public void onCreate() {
         super.onCreate();
         appComponent = DaggerAppComponent.builder()
                 .appModule(new AppModule(this))
                 .build();
         appComponent.inject(this);
+
+        if (BuildConfig.USE_CRASHLYTICS) {
+            Fabric.with(this, new Crashlytics());
+        }
     }
 
     public static App getApplication(Context context) {
